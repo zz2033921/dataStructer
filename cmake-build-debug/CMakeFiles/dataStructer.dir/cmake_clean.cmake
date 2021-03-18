c8ee3e2cafdcb9db5240df656d4547742abe3db4
姚政 <yz@yaozhengdeMacBook-Pro.local>
2021-03-18 23:29:13 +0800

    初始化git

---
+++ cmake-build-debug/CMakeFiles/dataStructer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dataStructer.dir/main.c.o"
+  "dataStructer"
+  "dataStructer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dataStructer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/dataStructer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dataStructer.
+# This may be replaced when dependencies are built.
