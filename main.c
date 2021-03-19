8e2ae1dd2d506fdb6dfe2d4ea12f9c3800be12d4
姚政 <yz@yaozhengdeMacBook-Pro.local>
2021-03-19 09:44:58 +0800

    add 初始化顺序表和 输出顺序表

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+//#define Size 5
+#define Size 5
+typedef struct Table{
+    int * head;
+    int length;
+    int size;
+}table;
+
+table initTable(){
+    table t;
+    t.head=(int *)malloc(Size* sizeof(int));//malloc 函数的作用是什么
+    if (!t.head){
+        printf("err");
+        exit(0);
+    }
+    t.length=0;
+    t.size=Size;
+    return t;
+}
+
+void displayTable(table t){
+    for (int i = 0; i < t.length; i++) {
+        printf("%d",t.head[i]);
+    }
+    printf("\n");
+}
 
 int main() {
     printf("Hello, World!\n");
+//    printf(Size);
+    table t =initTable();
+    printf("%d", sizeof(t.head));
+    for(int i=1;i<=Size;i++){
+        t.head[i-1]=i;
+        t.length++;
+    }
+    displayTable(t);
     return 0;
 }
+
+
