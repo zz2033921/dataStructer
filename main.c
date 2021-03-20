f1330efce5c4149b34355275bb83274f74e20236
姚政 <yz@yaozhengdeMacBook-Pro.local>
2021-03-20 23:30:35 +0800

    add some function

--- main.c
+++ main.c
@@ -1,43 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#define Size 5
+
 #define Size 5
-typedef struct Table{
-    int * head;
+
+typedef struct Table {
+    int *Head;
     int length;
     int size;
-}table;
+} table;
 
-table initTable(){
+table initTable() {
     table t;
-    t.head=(int *)malloc(Size* sizeof(int));//malloc 函数的作用是什么
-    if (!t.head){
+    t.Head = (int *) malloc(Size * sizeof(int));
+    if (!t.Head) {
         printf("err");
         exit(0);
     }
-    t.length=0;
-    t.size=Size;
+    t.length = 0;
+    t.size = Size;
     return t;
 }
 
-void displayTable(table t){
+void displayFun(table t) {
     for (int i = 0; i < t.length; i++) {
-        printf("%d",t.head[i]);
+        printf("%d\t", t.Head[i]);
     }
     printf("\n");
 }
 
-int main() {
-    printf("Hello, World!\n");
-//    printf(Size);
-    table t =initTable();
-    printf("%d", sizeof(t.head));
-    for(int i=1;i<=Size;i++){
-        t.head[i-1]=i;
-        t.length++;
+table insertTable(table t, int elem, int index) {
+//    printf("aaaaa");
+    if (index > t.length + 1 || index < 1) {
+        printf("err index");
+        return t;
     }
-    displayTable(t);
-    return 0;
+
+    if (t.length = t.size) {
+        t.Head = realloc(t.Head, (t.size + 1) * sizeof(int));
+        if (!t.Head) {
+            printf("err");
+            exit(0);
+        }
+    }
+
+    for (int i = t.length - 1; i >= index - 1; i--) {
+        t.Head[i + 1] = t.Head[i];
+    }
+    t.Head[index - 1] = elem;
+    t.length++;
+    return t;
+}
+
+table deleteTable(table t,int index){
+    if (index > t.length + 1 || index < 1) {
+        printf("err index");
+        return t;
+    }
+
+    for (int i=index-1;i<=t.length-1;i++){
+        t.Head[i]=t.Head[i+1];
+    }
+    t.length--;
+    return t;
+}
+int findTable(table t,int elem){
+    for (int i=0;i<t.length;i++){
+        if (t.Head[i]==elem){
+            return i+1;
+        } else{
+            return -1;
+        }
+    }
 }
 
 
+int main() {
+    printf("Hello, World!\n");
+    table c = initTable();
+    for (int i = 0; i < 5; i++) {
+        c.Head[i] = i+1;
+        c.length++;
+    }
+    c=insertTable(c,1111,6);
+    c=deleteTable(c,6);
+    displayFun(c);
+    return 0;
+}
